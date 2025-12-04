31194e8a8cebf8ff30cc3b38a801b83d41c7a5ca
Shovon96 <fakhruddinshovon@gmail.com>
2025-12-04 17:24:50 +0600

    frontend clone and setup as blank and install dependencies

---
+++ event-activities-frontend/src/app/(commonLayout)/page.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Events & Activities</title>
+        <meta
+          name="description"
+          content="Events & Activities Platform for Event Organizers and Attendees. Discover, book, and manage your events and activities effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Events & Activities</h1>
+      </main>
+    </>
+  );
+}
