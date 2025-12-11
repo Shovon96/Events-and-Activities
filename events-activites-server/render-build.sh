e810933725f322ac0e85db2158c9c509abfd0a6f
Shovon96 <fakhruddinshovon@gmail.com>
2025-12-12 03:37:01 +0600

    update

--- events-activites-server/render-build.sh
+++ events-activites-server/render-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+npm install @types/express @types/cors @types/cookie-parser @types/multer @types/jsonwebtoken @types/bcrypt
 npm install
 npx prisma generate
 npm run build
